40d19b14827df2fb0794a2c0a4e1303925475cc9
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2023-11-15 21:15:38 -0300

    Controllers and Dtos at Ninjas

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // GET /ninjas?type=fast --> []
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    return [{ type }];
+  }
+
+  // GET /ninjas/:id --> { ... }
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  // POST /ninjas
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name,
+    };
+  }
+
+  // PUT /ninjas/:id --> { ... }
+  @Put()
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return {
+      id,
+      name: updateNinjaDto,
+    };
+  }
+
+  // DELETE /ninjas/:id
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return {};
+  }
+}
